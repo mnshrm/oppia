c32c15b21ac56639cfbb2a7a255934b5fb62acd7
Aman Sharma <asysnsrs665@gmail.com>
2026-03-14 22:12:11 +0530

    update locators

--- core/tests/puppeteer-acceptance-tests/utilities/user/curriculum-admin.ts
+++ core/tests/puppeteer-acceptance-tests/utilities/user/curriculum-admin.ts
@@ -200,7 +200,7 @@ const viewTopicGraphButton = 'button.view-graph-button';
 const topicDependencyGraphDiv = '.e2e-test-topic-dependency-graph-container';
 const topicNode = '.e2e-test-topic-node';
 const closeTopicDependencyButton = '.e2e-test-close-topic-dependency-modal';
-const addTopicFormFieldInput = '.mat-select-search-input';
+const addTopicFormFieldInput = '.e2e-test-classroom-new-topic-add';
 const createNewTopicButton = '.e2e-test-create-topic-button';
 const createNewTopicMobileButton = '.e2e-test-create-topic-mobile-button';
 
