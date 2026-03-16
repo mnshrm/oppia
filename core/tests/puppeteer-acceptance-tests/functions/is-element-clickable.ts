5bc0f02d85093957d8fad36077bfa0e5ebaaefb3
Aman Sharma <asysnsrs665@gmail.com>
2026-03-17 00:07:33 +0530

    update

--- core/tests/puppeteer-acceptance-tests/functions/is-element-clickable.ts
+++ core/tests/puppeteer-acceptance-tests/functions/is-element-clickable.ts
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import { isElement } from "lodash";
+
 /**
  * @fileoverview Utility function to check if an element is clickable.
  */
